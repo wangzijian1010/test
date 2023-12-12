50412127d05e53f33277d3b7b4d54e608f393218
wangzijian <wangyuyan1022@gmail.com>
2023-12-12 20:40:41 +0800

    interface done

--- interface/emptyInterface/main.go
+++ interface/emptyInterface/main.go
@@ -10,6 +10,10 @@ import "fmt"
 type xxx interface {
 }
 
+// 1.空接口类型作为函数的参数就可以实现接收任何变量
+
+// 2.空接口可以作为map的value 可以实现map的value的任意类型
+
 func main() {
 	// 任意类型都可以存储
 	var x interface{}
@@ -21,5 +25,11 @@ func main() {
 
 	x = false
 	fmt.Println(x)
+
+	var m = make(map[string]interface{}, 16)
+	m["name"] = "娜扎"
+	m["age"] = 17
+	m["hobby"] = []string{"篮球", "足球"}
+	fmt.Println(m)
 
 }
